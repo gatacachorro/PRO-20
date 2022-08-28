f36a734a0be7f3f870e5539161361bc59da1f7d0
gatacachorro <100207223+gatacachorro@users.noreply.github.com>
2022-08-27 21:09:00 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Crie os Corpos Aqui.
+	
+	Engine.update(engine);
+	Engine.run(engine);
+
+	//criado corpos do plano e bloco 
+	var plane_options={
+		isStatic:true 
+	}
+	
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(verde);
+  
+  drawSprites();
+ 
+  block1 = Bodies.circle(220,10,10,block1_options);
+  World.add(world,block1);
+
+  block2 = Bodies.retangle(110,50,10,10,block2_options);
+  World.add(world,block2);
+
+  block3 = Bodies.rectangle(350,50,10,10,block3_options);
+  World.add(world,block3);
+  
+  var block1_options = { 
+	restitution:0.5,
+	friction:0.02,
+	frictionAir:0
+
+  }
+
+  var block2_options = {
+	restitution:0.7,
+	friction:0.01,
+	frictionAir:0.1
+
+  }
+
+  var block3_options = {
+	restitution:0.01,
+	friction:1,
+	frictionAir:0.3
+  }
+}
+
+
+
